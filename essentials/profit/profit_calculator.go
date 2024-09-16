225b86ff07c2fd71d3110ffacb2269ef5c321949
Vitor <viisanches98@gmail.com>
2024-09-16 19:42:39 -0300

    essentials/profit: add input validation and save result to file

--- essentials/profit/profit_calculator.go
+++ essentials/profit/profit_calculator.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const profitFile = "data/profit.txt";
 
 func calculateEBT(revenue float64, expenses float64) float64 {
 	return revenue - expenses;
@@ -14,17 +19,38 @@ func calculateRatio(ebt float64, profit float64) float64 {
 	return ebt / profit;
 }
 
+func valid(value float64) bool {
+	return value > 0
+}
+
+func writeResultsTofile(ebt, profit, ratio float64) error {
+	formattedText := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f", ebt, profit, ratio);
+	return os.WriteFile(profitFile, []byte(formattedText), 0644);
+}
+
 func main() {
 	var revenue, expenses, taxRate float64;
 
 	fmt.Print("Revenue: ");
 	fmt.Scan(&revenue);
+	if (!valid(revenue)) {
+		fmt.Println("revenue input is invalid");
+		return;
+	}
 
 	fmt.Print("Expenses: ");
 	fmt.Scan(&expenses);
+	if (!valid(expenses)) {
+		fmt.Println("expenses input is invalid");
+		return;
+	}
 
 	fmt.Print("Tax Rate (%): ");
 	fmt.Scan(&taxRate);
+	if (!valid(taxRate)) {
+		fmt.Println("tax rate input is invalid");
+		return;
+	}
 
 	ebt := calculateEBT(revenue, expenses);
 	profit := calculateProfit(ebt, taxRate);
@@ -33,4 +59,9 @@ func main() {
 	fmt.Println("EBT: ", ebt);
 	fmt.Println("Profit: ", profit);
 	fmt.Println("Ratio: ", ratio);
+
+	error := writeResultsTofile(ebt, profit, ratio);
+	if (error != nil) {
+		panic(error);
+	}
 }
